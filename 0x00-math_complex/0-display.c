8d6459a70452c87fec7d84f569a4a14113f17768
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-04-26 14:25:14 -0500

    Update 0-display.c

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -10,21 +10,39 @@
 
 void display_complex_number(complex c)
 {
-  printf("%.9g", c.re);
-  if (c.im)
-    {
-      if (c.im > 0)
+	if (c.im != 0 && c.re != 0)
 	{
-	  printf(" + ");
+		if (c.im == 1 || c.im == -1)
+		{
+			if (c.im < 0)
+				printf("%.0f - i\n", c.re);
+			else
+				printf("%.0f + i\n", c.re);
+		}
+		else
+		{
+			if (c.im < 0)
+				printf("%.0f - %.0fi\n", c.re, -c.im);
+			else
+				printf("%.0f + %.0fi\n", c.re, c.im);
+		}
 	}
-      else
+	else if (c.im == 0 && c.re != 0)
+		printf("%.0f\n", c.re);
+
+	else if (c.im != 0 && c.re == 0)
 	{
-	  printf(" - ");
-	  c.im *= -1;
+		if (c.im == 1 || c.im == -1)
+		{
+			if (c.im < 0)
+				printf("-i\n");
+			else
+				printf("i\n");
+		}
+		else
+			printf("%.0fi\n", c.im);
 	}
-      if (c.im != 1 && c.im != -1)
-	printf("%.9g", c.im);
-      printf("i");
-    }
-  printf("\n");
+
+	if (c.im == 0 && c.re == 0)
+		printf("%d\n", 0);
 }
