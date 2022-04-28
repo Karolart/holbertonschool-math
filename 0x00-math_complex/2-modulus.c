a7be4acd0acbe4c5a1774e2188eb64a90cfe948f
karolart <karoljuzz1610@gmail.com>
2022-04-28 03:38:02 +0000

    function that returns the modulus of a given complex number

---
+++ 0x00-math_complex/2-modulus.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* double modulus(complex c)- returns the modulus of a given complex number
+*@c: Complex Number
+*
+/
+
+ double modulus(complex c)
+{
+        double modulo;
+
+	modulo = sqrt(pow(c.re, 2) + pow(c.im, 2));
+	return (modulo);
+}
+
+ 
